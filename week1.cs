d16cc7547fc14277ae7405ee00cde52d49d9627d
hectorvzp <elderzambranoperez@gmail.com>
2025-03-18 14:42:31 -0300

    df

---
+++ week1.cs
@@ -0,0 +1,34 @@
+using System;
+class Program
+{
+
+
+
+  int x;
+  x = 10;
+console.Writeline(x)
+ int y = 20
+console.Writeline(y)
+
+Console.Write("There will not be a newline after this.");
+  Console.Write("What is your favorite color? ");
+string color = Console.ReadLine();
+
+if (x > y)
+{
+  Console.WriteLine("greater");
+}
+
+string school = "BYU-Idaho";
+Console.WriteLine($"I am studying at {school}.");}
+
+
+string valueInText = "42";
+int number = int.Parse(valueInText);
+
+Console.Write("What is your favorite number? ");
+string userInput = Console.ReadLine();
+int number = int.Parse(userInput);
+
+int number = 42;
+string textVersion = number.ToString();
